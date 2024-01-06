70786e29503f0d8a8268a18776c9d9ca3f15f5fd
Petrk12 <petrkutil123@gmail.com>
2024-01-06 16:17:26 +0100

    Client side error handling added

--- API/Controllers/BaseController.cs
+++ API/Controllers/BaseController.cs
@@ -15,13 +15,13 @@ public class BaseController : ControllerBase
 
     protected ActionResult HandleResult<T>(Result<T> result)
     {
-        if (result == null) return NotFound();
+        if (result == null) return NotFound(new ApiResponse(404));
 
         if (result.IsSuccess && result.Value != null) return Ok(result.Value);
 
         if (result.IsSuccess && result.Value == null) return NotFound(new ApiResponse(404));
         
-        return BadRequest();
+        return  BadRequest(new ApiResponse(400));
     }
 
 
--- API/Program.cs
+++ API/Program.cs
@@ -1,20 +1,15 @@
 using API.Errors;
 using API.Extensions;
 using API.Middleware;
-using Application.MappingProfiles;
-using Application.Products;
-using Domain.Repository;
-using Infrastructure.DataAccess.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Infrastructure.Persistence;
 using Microsoft.AspNetCore.Mvc;
-using Swashbuckle.AspNetCore.SwaggerUI;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddApplicationServices(builder.Configuration);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddApplicationServices(builder.Configuration);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
